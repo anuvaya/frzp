50c9ff2286ea75a3f96a27bab17b9dbbab55cb12
Nitesh Kumar Niranjan <hello@theux.dev>
2025-05-14 21:14:28 +0530

    refactor: update FlexibleRazorPayModule to return success response
    - Modified the open method to return a SuccessResponse object containing razorpay_signature, razorpay_order_id, and razorpay_payment_id.
    - Enhanced TypeScript definitions for better type safety and clarity.

--- src/FlexibleRazorPay.types.ts
+++ src/FlexibleRazorPay.types.ts
@@ -1,17 +1,80 @@
-import type { StyleProp, ViewStyle } from "react-native"
-
-export type OnLoadEventPayload = {
-  url: string
-}
-
 export type FlexibleRazorPayModuleEvents = {}
 
-export type ChangeEventPayload = {
-  value: string
+export interface CheckoutOptions {
+  key: string
+  amount: number
+  currency: string
+  name: string
+  description: string
+  image?: string
+  order_id: string
+  prefill?: {
+    name?: string
+    email?: string
+    contact?: string
+    method?: "card" | "netbanking" | "wallet" | "emi" | "upi"
+  }
+  notes?: Record<string | number, string>
+  theme?: {
+    hide_topbar?: boolean
+    color?: string
+    backdrop_color?: string
+  }
+  modal?: {
+    backdropclose?: boolean
+    escape?: boolean
+    handleback?: boolean
+    confirm_close?: boolean
+    ondismiss?: () => void
+    animation?: boolean
+  }
+  subscription_id?: string
+  subscription_card_change?: boolean
+  recurring?: boolean
+  callback_url?: string
+  redirect?: boolean
+  customer_id?: string
+  timeout?: number
+  remember_customer?: boolean
+  readonly?: {
+    contact?: boolean
+    email?: boolean
+    name?: boolean
+  }
+  hidden?: {
+    contact?: boolean
+    email?: boolean
+  }
+  send_sms_hash?: boolean
+  allow_rotation?: boolean
+  retry?: {
+    enabled: boolean
+    max_count: number
+  }
+  config?: {
+    display: {
+      language: "en" | "ben" | "hi" | "mar" | "guj" | "tam" | "tel"
+    }
+  }
 }
 
-export type FlexibleRazorPayViewProps = {
-  url: string
-  onLoad: (event: { nativeEvent: OnLoadEventPayload }) => void
-  style?: StyleProp<ViewStyle>
+export interface SuccessResponse {
+  razorpay_signature: string
+  razorpay_order_id: string
+  razorpay_payment_id: string
+}
+
+export interface ErrorResponse {
+  code: number
+  description: string
+  error: {
+    field?: string
+    source: string
+    step: string
+    reason: string
+    metadata: {
+      payment_id?: string
+      order_id: string
+    }
+  }
 }
--- src/FlexibleRazorPayModule.ts
+++ src/FlexibleRazorPayModule.ts
@@ -1,68 +1,13 @@
 import { NativeModule, requireNativeModule } from "expo"
 
-import { FlexibleRazorPayModuleEvents } from "./FlexibleRazorPay.types"
-
-interface CheckoutOptions {
-  key: string
-  amount: number
-  currency: string
-  name: string
-  description: string
-  image?: string
-  order_id: string
-  prefill?: {
-    name?: string
-    email?: string
-    contact?: string
-    method?: "card" | "netbanking" | "wallet" | "emi" | "upi"
-  }
-  notes?: Record<string | number, string>
-  theme?: {
-    hide_topbar?: boolean
-    color?: string
-    backdrop_color?: string
-  }
-  modal?: {
-    backdropclose?: boolean
-    escape?: boolean
-    handleback?: boolean
-    confirm_close?: boolean
-    ondismiss?: () => void
-    animation?: boolean
-  }
-  subscription_id?: string
-  subscription_card_change?: boolean
-  recurring?: boolean
-  callback_url?: string
-  redirect?: boolean
-  customer_id?: string
-  timeout?: number
-  remember_customer?: boolean
-  readonly?: {
-    contact?: boolean
-    email?: boolean
-    name?: boolean
-  }
-  hidden?: {
-    contact?: boolean
-    email?: boolean
-  }
-  send_sms_hash?: boolean
-  allow_rotation?: boolean
-  retry?: {
-    enabled: boolean
-    max_count: number
-  }
-  config?: {
-    display: {
-      language: "en" | "ben" | "hi" | "mar" | "guj" | "tam" | "tel"
-    }
-  }
-}
+import {
+  CheckoutOptions,
+  FlexibleRazorPayModuleEvents,
+  SuccessResponse,
+} from "./FlexibleRazorPay.types"
 
 declare class FlexibleRazorPayModule extends NativeModule<FlexibleRazorPayModuleEvents> {
-  open(options: CheckoutOptions): Promise<void>
+  open(options: CheckoutOptions): Promise<SuccessResponse>
 }
 
-// This call loads the native module object from the JSI.
 export default requireNativeModule<FlexibleRazorPayModule>("FlexibleRazorPay")
